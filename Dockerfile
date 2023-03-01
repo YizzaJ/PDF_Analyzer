63b2fedfae3a983d635a8f1d5e016c45ced3a5c3
Jesus Hernandez <jesusdavidhernandez.perez@gmail.com>
2023-03-01 21:40:45 +0100

    Made changes to the way informations is displayed.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
 FROM python:3.9
 
-RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-
 WORKDIR /app
 
 COPY . /app
 
-RUN /root/.poetry/bin/poetry install --no-dev
 
 CMD ["python", "practica_1/analyze.py"]
--- practica_1/extract.py
+++ practica_1/extract.py
@@ -1,37 +1,43 @@
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
 import os
+
 
 def get_keywords(soup):
     keywords = soup.find('keywords')
     if keywords is not None:
-        return "### Keywords:\n" + keywords.text
+        return "<h3>Keywords: </h3> <p>" + keywords.text + "</p>"
     else:
-        return "### No keywords found.\n"
+        return "<h3>Keywords: </h3> <p>No Keywords found.</p>"
 
 def get_figures(soup):
     figures = soup.find_all('figure')
     if figures is not None:
-        return "### Number of figures found: " + str(len(figures)) + "\n"
+        return len(figures)
     else:
-       return "### No figures found.\n\n"
+       return 0
     
 
 def get_links(soup):
     links = soup.find_all("ptr")
     res = ""
     if str(len(links)) != 0:
-        res += "### Links found (" + str(len(links)) + "):\n"
+        res += "<h3>Links found (" + str(len(links)) + "):<h3><ul>"
         for link in links:
-            res += "- " + link["target"] + "\n"
+            res += "<li style=\"font-size:18px\"><a href=\"" + link["target"] + "\">" + link["target"] + "</a></li>"
+        res += "</ul>"
     else:
-        res= "### No links found.\n"
+        res= "<h3>No links found.<h3>"
     return res
 
 output_path = '../pdfs/output/'
-output_file = '../rationale.md'
+output_file = '../analysis.html'
 
 if os.path.exists(output_file):
     os.remove(output_file)
+
+num_figures = [] # list to store the number of figures for each file
+file_names = [] # list to store the name of each file
 
 with open(output_file, 'w') as file:
     for root, dirs, files in os.walk(output_path):
@@ -40,15 +46,22 @@ with open(output_file, 'w') as file:
 
             with open(file_path, 'r', encoding="utf-8") as tei:
                 soup = BeautifulSoup(tei, 'xml')
-                keywords = get_keywords(soup)
-                figures = get_figures(soup)
-                links = get_links(soup)
-
-                file.write("# For file: " + str(file_path).removeprefix("../pdfs/output/") + "\n")
+                file_display = str(file_path).removeprefix("../pdfs/output/").removesuffix(".tei.xml")
+                file.write("<h1> For file: " + file_display + "</h1>")
                 
                 file.write(get_keywords(soup))
-                file.write(get_figures(soup))
-
+                num_figures.append(get_figures(soup))
+                file_names.append(file_display)
                 file.write(get_links(soup))
 
-                file.write("---\n")
+                file.write("<hr></hr>")
+
+    plt.figure()
+    plt.bar(num_figures, file_names,align='center') 
+    plt.xlabel('File names')
+    plt.ylabel('Number of figures')
+    plt.xticks(rotation=90)
+    plt.savefig('figures_histogram.png')
+
+    file.write("<h1>Number of Figures Histogram:</h1>")
+    file.write("<img src=\"figures_histogram.png\">")
